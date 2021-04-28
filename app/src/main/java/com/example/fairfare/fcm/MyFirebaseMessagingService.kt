974511c9b1c8ee0135e066b14a30a572ebd01e3b
Mohsin Sheikh <mohsin.sheikh16@gmail.com>
2021-04-28 12:51:43 +0530

    Notification issue done.

--- app/src/main/java/com/example/fairfare/fcm/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/example/fairfare/fcm/MyFirebaseMessagingService.kt
@@ -6,6 +6,7 @@
  */
 package com.example.fairfare.fcm
 
+import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -15,12 +16,16 @@ import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.widget.RemoteViews
 import androidx.core.app.NotificationCompat
 import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.fairfare.R
+import com.example.fairfare.ui.Login.LoginActivity
 import com.example.fairfare.ui.home.HomeActivity
+import com.example.fairfare.utils.Constants
+import com.example.fairfare.utils.PreferencesManager
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import org.json.JSONException
@@ -33,6 +38,23 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     private var message: String? = null
     private var notifyType: String? = null
     private var request = ""
+    private var broadcaster: LocalBroadcastManager? = null
+    var isLogin: String? = null
+    var mPreferencesManager: PreferencesManager? = null
+    var CHANNEL_ID = "1"
+    private var id = 0
+
+    override fun onCreate() {
+        PreferencesManager.initializeInstance(this)
+        mPreferencesManager = PreferencesManager.instance
+        broadcaster = LocalBroadcastManager.getInstance(this)
+        isLogin = mPreferencesManager!!.getStringValue(Constants.SHARED_PREFERENCE_ISLOGIN)
+    }
+
+    override fun onNewToken(p0: String) {
+        super.onNewToken(p0)
+        Log.e("NEW_TOKEN", p0)
+    }
 
 
 
@@ -40,14 +62,24 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         Log.e("FCMqw", remoteMessage.data.toString())
         Log.e("FCMNoti", remoteMessage.toString())
 
-
-
-        try {
+       /* // Check if message contains a data payload.
+        if (remoteMessage.data.size > 0) {
+            try {
                 handleDataMessage(remoteMessage)
             } catch (e: Exception) {
                 Log.e("JSONError", e.toString())
             }
+        }
 
+        if (remoteMessage.notification != null) {
+            try {
+                handleDataMessage(remoteMessage)
+            } catch (e: Exception) {
+                Log.e("JSONError", e.toString())
+            }
+        }*/
+
+        showChatNotification(remoteMessage)
     }
 
     private fun handleDataMessage(json: RemoteMessage) {
@@ -63,6 +95,77 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
             Log.e(getString(R.string.app_name), "Json Exception: " + e.message)
         } catch (e: Exception) {
             Log.e(getString(R.string.app_name), "Exception: " + e.message)
+        }
+    }
+
+    private fun showChatNotification(remoteMessage: RemoteMessage) {
+        val title = remoteMessage.data["title"]
+        val message = remoteMessage.data["body"]
+        val screen = remoteMessage.data["screen"]
+        var notificationIntent: Intent? = null
+        var pendingIntent: PendingIntent? = null
+
+        val intent = Intent(applicationContext, HomeActivity::class.java)
+        //intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        //  intent.putExtra("AnotherActivity", screen);
+
+
+        val builder =
+            NotificationCompat.Builder(applicationContext, CHANNEL_ID)
+                .setSmallIcon(R.mipmap.ic_launcher_round)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setSound(Settings.System.DEFAULT_NOTIFICATION_URI)
+                .setVibrate(longArrayOf(1000, 1000, 1000, 1000, 1000))
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setPriority(NotificationCompat.PRIORITY_MAX)
+        //.setStyle(new NotificationCompat.BigPictureStyle()
+        // .bigPicture(myBitmap));
+        if (isLogin == "true") {
+            notificationIntent = Intent(
+                applicationContext,
+                HomeActivity::class.java)
+            notificationIntent.action = "MyRides"
+
+                if (notificationIntent != null) {
+                    notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                    pendingIntent = PendingIntent.getActivity(
+                        this,
+                        0,
+                        notificationIntent,
+                        PendingIntent.FLAG_ONE_SHOT
+                    )
+                }
+            } else {
+            notificationIntent = Intent(applicationContext, LoginActivity::class.java)
+            notificationIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            pendingIntent =
+                PendingIntent.getActivity(this, 0, notificationIntent, PendingIntent.FLAG_ONE_SHOT)
+        }
+        builder.setContentIntent(pendingIntent)
+        builder.setAutoCancel(true)
+        sendNotification(builder)
+    }
+
+
+    private fun sendNotification(builder: NotificationCompat.Builder) {
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        if (notificationManager != null) {
+            // Since android Oreo notification channel is needed.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channel = NotificationChannel(
+                    CHANNEL_ID,
+                    "1",
+                    NotificationManager.IMPORTANCE_HIGH
+                )
+                notificationManager.createNotificationChannel(channel)
+            }
+            notificationManager.notify(
+                id++ /* ID of notification */,
+                builder.build()
+            )
         }
     }
 
@@ -121,6 +224,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         bundle.putString("request", request)
         bundle.putString("notificationId", notificationId)
         intent.putExtras(bundle)
-        LocalBroadcastManager.getInstance(this).sendBroadcast(intent)
+        broadcaster?.sendBroadcast(intent)
     }
 }
