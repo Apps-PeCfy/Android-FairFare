8763b57d8faf56ebd0f69836217a634034d17b46
Mohsin Sheikh <mohsin.sheikh16@gmail.com>
2021-11-27 15:43:19 +0530

    IntercityDispute Detail

---
+++ app/src/main/java/com/fairfareindia/ui/drawer/intercitydispute/InterCityDisputeDetailsActivity.kt
@@ -0,0 +1,12 @@
+package com.fairfareindia.ui.drawer.intercitydispute
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fairfareindia.R
+
+class InterCityDisputeDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inter_city_dispute_details)
+    }
+}
